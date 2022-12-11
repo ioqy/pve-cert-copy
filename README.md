05e62de96ef54a533feb1ddb895292bb52f306a9
ioqy <95605581+ioqy@users.noreply.github.com>
2022-12-11 14:40:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# A systemd timer to automatically copy the SSL certificate from Proxmox Virtual Environment to Proxmox Backup Server
+
+
+## Install
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/ioqy/pve-cert-copy/master/install.sh | sudo sh
+```
+
+## Uninstall
+
+```bash
+sudo /usr/local/bin/uninstall-pve-cert-copy.sh
+```
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+if [ "$(whoami)" != "root" ]; then
+  echo Script must be run as root
+  exit 1
+fi
+
+systemctl disable --now pve-cert-copy.timer 2>/dev/null
+
+cat << EOF > /usr/local/bin/pve-cert-copy.sh
+#!/bin/sh
+if [ "\$(whoami)" != "root" ]; then
+  echo Script must be run as root
+  exit 1
+fi
+NODE=\$(hostname)
+cp /etc/pve/nodes/\${NODE}/pveproxy-ssl.pem /etc/proxmox-backup/proxy.pem
+cp /etc/pve/nodes/\${NODE}/pveproxy-ssl.key /etc/proxmox-backup/proxy.key
+chmod 640 /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
+chgrp backup /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
+systemctl reload proxmox-backup-proxy.service
+EOF
+
+cat << EOF > /etc/systemd/system/pve-cert-copy.service
+[Unit]
+Description=Copy PVE certificate for proxmox backup server
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/copy-pve-cert.sh
+EOF
+
+cat << EOF > /etc/systemd/system/pve-cert-copy.timer
+[Unit]
+Description=Copy PVE certificate for proxmox backup server
+[Timer]
+OnCalendar=*-*-* 04:00:00
+RandomizedDelaySec=1h
+Persistent=true
+[Install]
+WantedBy=timers.target
+EOF
+
+cat << EOF > /usr/local/bin/uninstall-pve-cert-copy.sh
+#!/bin/sh
+if [ "\$(whoami)" != "root" ]; then
+  echo Script must be run as root
+  exit 1
+fi
+systemctl disable --now pve-cert-copy.timer 2>/dev/null
+rm /etc/systemd/system/pve-cert-copy.service
+rm /etc/systemd/system/pve-cert-copy.timer
+systemctl daemon-reload 2>/dev/null
+rm /usr/local/bin/uninstall-pve-cert-copy.sh
+EOF
+
+chmod +x /usr/local/bin/pve-cert-copy.sh
+chmod +x /usr/local/bin/uninstall-pve-cert-copy.sh
+
+systemctl daemon-reload
+systemctl enable --now pve-cert-copy.timer 2>/dev/null
