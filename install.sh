48c00a499da0ce975ac56f856c8b3db3f91f2f04
ioqy <95605581+ioqy@users.noreply.github.com>
2023-01-21 15:52:37 +0100

    Use path unit to monitor changes to the certificate

--- install.sh
+++ install.sh
@@ -5,19 +5,9 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 fi
 
-cat << EOF > /usr/local/bin/pve-cert-copy.sh
-#!/bin/sh
-if [ "\$(whoami)" != "root" ]; then
-  echo Script must be run as root
-  exit 1
+if [ -e "/usr/local/bin/uninstall-pve-cert-copy.sh" ]; then
+  /usr/local/bin/uninstall-pve-cert-copy.sh
 fi
-NODE=\$(hostname)
-cp /etc/pve/nodes/\${NODE}/pveproxy-ssl.pem /etc/proxmox-backup/proxy.pem
-cp /etc/pve/nodes/\${NODE}/pveproxy-ssl.key /etc/proxmox-backup/proxy.key
-chmod 640 /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
-chgrp backup /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
-systemctl reload proxmox-backup-proxy.service
-EOF
 
 cat << EOF > /etc/systemd/system/pve-cert-copy.service
 [Unit]
@@ -25,9 +15,23 @@ Description=Copy PVE certificate for proxmox backup server
 After=pve-daily-update.service
 [Service]
 Type=oneshot
-ExecStart=/usr/local/bin/pve-cert-copy.sh
+ExecStart=/usr/bin/cp /etc/pve/nodes/$(hostname)/pveproxy-ssl.pem /etc/proxmox-backup/proxy.pem
+ExecStart=/usr/bin/cp /etc/pve/nodes/$(hostname)/pveproxy-ssl.key /etc/proxmox-backup/proxy.key
+ExecStart=/usr/bin/chmod 640 /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
+ExecStart=/usr/bin/chgrp backup /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
+ExecStart=/usr/bin/systemctl reload proxmox-backup-proxy.service
 [Install]
-WantedBy=pve-daily-update.service
+WantedBy=pveproxy.service
+EOF
+
+cat << EOF > /etc/systemd/system/pve-cert-copy.path
+[Unit]
+Description=Copy PVE certificate for proxmox backup server
+[Path]
+PathChanged=/etc/pve/nodes/pm/pve-ssl.pem
+Unit=pve-cert-copy.service
+[Install]
+WantedBy=multi-user.target
 EOF
 
 cat << EOF > /usr/local/bin/uninstall-pve-cert-copy.sh
@@ -37,14 +41,12 @@ if [ "\$(whoami)" != "root" ]; then
   exit 1
 fi
 systemctl disable pve-cert-copy.service
-rm /etc/systemd/system/pve-cert-copy.service
+systemctl disable --now pve-cert-copy.path
 systemctl daemon-reload 2>/dev/null
-rm /usr/local/bin/pve-cert-copy.sh
 rm /usr/local/bin/uninstall-pve-cert-copy.sh
 EOF
 
-chmod +x /usr/local/bin/pve-cert-copy.sh
-chmod +x /usr/local/bin/uninstall-pve-cert-copy.sh
-
 systemctl daemon-reload
+systemctl enable --now pve-cert-copy.path
+# Run once after installation
 systemctl enable --now pve-cert-copy.service
