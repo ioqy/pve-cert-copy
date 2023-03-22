dd4145ecda44c52ef615a5d97be2923a5b5c828a
ioqy <95605581+ioqy@users.noreply.github.com>
2023-03-22 21:00:31 +0100

    Bug fixes and removal of unneeded things

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ fi
 
 cat << EOF > /etc/systemd/system/pve-cert-copy.service
 [Unit]
-Description=Copy PVE certificate for proxmox backup server
-After=pve-daily-update.service
+Description=Copy the certificate from Proxmox Virtual Environment to Proxmox Backup Server
 [Service]
 Type=oneshot
 ExecStart=/usr/bin/cp /etc/pve/nodes/$(hostname)/pveproxy-ssl.pem /etc/proxmox-backup/proxy.pem
@@ -20,15 +19,13 @@ ExecStart=/usr/bin/cp /etc/pve/nodes/$(hostname)/pveproxy-ssl.key /etc/proxmox-b
 ExecStart=/usr/bin/chmod 640 /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
 ExecStart=/usr/bin/chgrp backup /etc/proxmox-backup/proxy.key /etc/proxmox-backup/proxy.pem
 ExecStart=/usr/bin/systemctl reload proxmox-backup-proxy.service
-[Install]
-WantedBy=pveproxy.service
 EOF
 
 cat << EOF > /etc/systemd/system/pve-cert-copy.path
 [Unit]
-Description=Copy PVE certificate for proxmox backup server
+Description=Watch the certificate from Proxmox Virtual Environment to Proxmox Backup Server
 [Path]
-PathChanged=/etc/pve/nodes/pm/pveproxy-ssl.pem
+PathChanged=/etc/pve/nodes/$(hostname)/pveproxy-ssl.pem
 Unit=pve-cert-copy.service
 [Install]
 WantedBy=multi-user.target
@@ -40,13 +37,16 @@ if [ "\$(whoami)" != "root" ]; then
   echo Script must be run as root
   exit 1
 fi
-systemctl disable pve-cert-copy.service
 systemctl disable --now pve-cert-copy.path
 systemctl daemon-reload 2>/dev/null
+rm /etc/systemd/system/pve-cert-copy.service
+rm /etc/systemd/system/pve-cert-copy.path
 rm /usr/local/bin/uninstall-pve-cert-copy.sh
 EOF
+
+chmod +x /usr/local/bin/uninstall-pve-cert-copy.sh
 
 systemctl daemon-reload
 systemctl enable --now pve-cert-copy.path
 # Run once after installation
-systemctl enable --now pve-cert-copy.service
+systemctl start pve-cert-copy.service
